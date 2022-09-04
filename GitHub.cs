312a705f4b4a64bfa3bbcf9101d91a76477b09b2
ComputerKim <computerkim@live.dk>
2022-09-04 21:31:33 +0200

    Update GitHub.cs

--- GitHub.cs
+++ GitHub.cs
@@ -7,6 +7,7 @@ using System.Text.RegularExpressions;
 public static class GitHub {
     public static string UserAgent;
     static readonly WebClient client = new();
+    static readonly Regex defaultBranchRegex = new("default_branch\":\"([^\"]*)", RegexOptions.Compiled);
     static readonly Regex branchesRegex = new("name\":\"([^\"]*)", RegexOptions.Compiled);
     static readonly Regex fileRegex = new("content\":\"([^\"]*)", RegexOptions.Compiled);
     static readonly Regex filesRegex = new("path\":\"([^\"]*).*?url\":\"([^\"]*)", RegexOptions.Compiled);
@@ -19,19 +20,19 @@ public static class GitHub {
 
     public static byte[] File(string url) => Get(url, fileRegex).Select(x => Convert.FromBase64String(x.Groups[1].Value.Replace("\\n", ""))).FirstOrDefault();
 
-    public static string Branch(string owner, string repo) => Branches(owner, repo).First();
+    public static string DefaultBranch(string owner, string repo) => Get($"https://api.github.com/repos/{owner}/{repo}", defaultBranchRegex).Select(x => x.Groups[1].Value).First();
 
     public static IEnumerable<string> Branches(string owner, string repo) =>
         Get($"https://api.github.com/repos/{owner}/{repo}/branches", branchesRegex).Select(x => x.Groups[1].Value);
 
     public static IEnumerable<(string path, string url)> Files(string owner, string repo) =>
-        Files(owner, repo, Branch(owner, repo));
+        Files(owner, repo, DefaultBranch(owner, repo));
 
     public static IEnumerable<(string path, string url)> Files(string owner, string repo, string version) =>
         Get($"https://api.github.com/repos/{owner}/{repo}/git/trees/{version}?recursive=1", filesRegex)
             .Select(x => (x.Groups[1].Value, x.Groups[2].Value));
 
-    public static byte[] Zip(string owner, string repo) => Zip(owner, repo, Branch(owner, repo));
+    public static byte[] Zip(string owner, string repo) => Zip(owner, repo, DefaultBranch(owner, repo));
 
     public static byte[] Zip(string owner, string repo, string version) =>
         client.DownloadData($"https://codeload.github.com/{owner}/{repo}/zip/{version}");
